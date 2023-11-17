b930b8b2637e4e4272d8632f70d30c220f29570f
osama mostafa <127859015+Osama0811@users.noreply.github.com>
2023-11-17 03:38:34 +0200

    Update app-routing.module.ts

--- Healthy-Care-Front/src/app/app-routing.module.ts
+++ Healthy-Care-Front/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './exceptions/page-not-found/page-not-found.component';
 
 const routes: Routes = [
    { path: '', redirectTo: 'Login', pathMatch: 'full' },
    {
     path: 'UserAdmin',
-    loadChildren:()=>import('src/app/admin/admin.module').then(m=>m.AdminModule) ,
+    loadChildren:()=>import('src/app/admin/Admin.module').then(m=>m.AdminModule) ,
     //canActivate: [adminGuard],
     //canLoad: [adminGuard]
   },
-   //{ path: '**', component: PageNotFoundComponent }
    {path: '**', component: PageNotFoundComponent }
 ];
 
