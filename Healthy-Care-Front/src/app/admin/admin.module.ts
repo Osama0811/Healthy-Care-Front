491d57ee8ec657b99e0c4572845710eb2125f83e
AlhmedElshiekh <152450591+AlhmedElshiekh@users.noreply.github.com>
2024-02-27 23:05:00 +0200

    .2

--- Healthy-Care-Front/src/app/admin/admin.module.ts
+++ Healthy-Care-Front/src/app/admin/admin.module.ts
@@ -17,7 +17,8 @@ import { QuestionHistoryComponent } from './Component/QuestionHistory/QuestionHi
 
 @NgModule({
 
-  imports: [ 
+  imports: [
+    
     CommonModule,
     AdminRoutingModule,
     FormsModule,
