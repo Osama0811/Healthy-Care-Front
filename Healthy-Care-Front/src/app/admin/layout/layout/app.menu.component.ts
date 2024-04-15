1577720a408b0f5adb96600ef4555b08564623c2
mhmdtnager <145150188+mhmdtnager@users.noreply.github.com>
2024-04-15 22:22:12 +0200

    order links

--- Healthy-Care-Front/src/app/admin/layout/layout/app.menu.component.ts
+++ Healthy-Care-Front/src/app/admin/layout/layout/app.menu.component.ts
@@ -14,274 +14,127 @@ export class AppMenuComponent implements OnInit {
 
     ngOnInit() {
         this.model = [
-            {
-                label: 'Home',
-                items: [
-                    { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
-                ]
-            },
-            {
-                label: 'Users',
-                items: [
-                    { label: 'Profile', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/Profile'] },
-                    { label: 'Index', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/Index'] },
-
-                    //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-                    //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-                ]
-            },
-            {
-              label: 'Blood',
-              items: [
-                  { label: 'Blood1', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/Blood'] },
-                  { label: 'Blood2', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/Blood2'] },
-
-                  //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-                  //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-              ]
-          },
           {
-            label: 'BloodBank',
+            label: 'System Setting',
+            icon: 'pi pi-spin pi-spinner',
             items: [
-                { label: 'BloodBank', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/BloodBank'] },
 
-                //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
+                {
+                    label: 'Users',
+                    icon: 'pi pi-fw pi-user',
+                    items: [
+                        {
+                          label: 'User', icon: 'pi pi-circle-fill', routerLink: ['/admin/User']
+                        },
+                        {
+                          label: 'Patient', icon: 'pi pi-circle-fill', routerLink: ['/admin/Patient']
+                        },
+                        { label: 'Doctor', icon: 'pi pi-circle-fill', routerLink: ['/admin/Doctor'] },
+                    ]
+                },
 
-                //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
+              {
+                label: 'Blood Setting',
+                icon: 'pi pi-spin pi-spinner',
+                items: [
+                  { label: 'Blood1', icon: 'pi pi-circle-fill', routerLink: ['/admin/Blood'] },
+                    { label: 'BloodEquation', icon: 'pi pi-circle-fill', routerLink: ['/admin/BloodEquation'] },
+                    { label: 'BloodBank', icon: 'pi pi-circle-fill', routerLink: ['/admin/BloodBank'] },
+                ]
+            },
+            { label: 'Address', icon: 'pi pi-flag', routerLink: ['/admin/Address'] }
             ]
-        },{
-          label: 'Category',
+        },
+        {
+          label: 'Hospital Setting',
+          icon: 'pi pi-spin pi-spinner',
           items: [
-              { label: 'Category', icon: 'pi pi-list', routerLink: ['/admin/Category'] },
 
-              //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-              //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
+              {
+                label: 'Hospital Setting',
+                icon: 'pi pi-fw pi-home',
+                items: [
+                    {
+
+                       label: 'Hospital', icon: 'pi pi-circle-fill', routerLink: ['/admin/Hospital']
+                    },
+                    { label: 'Category', icon: 'pi pi-circle-fill', routerLink: ['/admin/Category'] },
+                    { label: 'Department', icon: 'pi pi-circle-fill', routerLink: ['/admin/Department'] },
+                    { label: 'Doctor_Hospital', icon: 'pi pi-circle-fill', routerLink: ['/admin/Doctor_Hospital'] },
+                    { label: 'Department_Hospital', icon: 'pi pi-circle-fill', routerLink: ['/admin/Department_Hospital'] },
+                    { label: 'Doctor_Department', icon: 'pi pi-circle-fill', routerLink: ['/admin/Doctor_Department'] },
+                ]
+            }
           ]
       }
-      ,{
-        label: 'Doctor',
-        items: [
-            { label: 'Doctor', icon: 'pi pi-list', routerLink: ['/admin/Doctor'] },
+            // {
+            //     label: 'Home',
+            //     items: [
+            //         { label: 'Dashboard', icon: 'pi pi-fw pi-home', routerLink: ['/'] }
+            //     ]
+            // },
+            // {
+            //     label: 'Users',
+            //     items: [
+            //         { label: 'Profile', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/Profile'] },
+            //         { label: 'Index', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/Index'] },
 
-            //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
+            //         //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-            //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
+            //         //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
-        ]
-    }
-    ,{
-      label: 'Doctor_Department',
-      items: [
-          { label: 'Doctor_Department', icon: 'pi pi-list', routerLink: ['/admin/Doctor_Department'] },
+            //     ]
+            // },
 
-          //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-          //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
-      ]
-  }
-  ,{
-    label: 'Doctor_Hospital',
-    items: [
-        { label: 'Doctor_Hospital', icon: 'pi pi-list', routerLink: ['/admin/Doctor_Hospital'] },
 
-        //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-        //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-    ]
-}
     ,{
       label: 'Tools',
       items: [
-          { label: 'Tools', icon: 'pi pi-list', routerLink: ['/admin/Tools'] },
-
-          //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-          //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
+          { label: 'Tools', icon: 'pi pi-spin pi-cog', routerLink: ['/admin/Tools'] },
+          { label: 'Tools_Department', icon: 'pi pi-wrench', routerLink: ['/admin/Tools_Department'] },
+          { label: 'Tools_Hospital', icon: 'pi pi-list', routerLink: ['/admin/Tools_Hospital'] },
 
       ]
-  }
-  ,{
-    label: 'Tools_Department',
-    items: [
-        { label: 'Tools_Department', icon: 'pi pi-list', routerLink: ['/admin/Tools_Department'] },
+  },
 
-        //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-        //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-    ]
-},{
-  label: 'Tools_Hospital',
-  items: [
-      { label: 'Tools_Hospital', icon: 'pi pi-list', routerLink: ['/admin/Tools_Hospital'] },
-
-      //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-      //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-  ]
-},
-
-            {
-                label: 'Blood',
-                icon: 'pi pi-fw pi-briefcase',
-                items: [
-
-                    {
-                        label: 'Blood',
-                        icon: 'pi pi-fw pi-user',
-                        items: [
-                            {
-                                label: 'Blood1',
-                                icon: 'pi pi-fw pi-sign-in',
-                                routerLink: ['/auth/login']
-                            },
-                            {
-                                label: 'Blood2',
-                                icon: 'pi pi-fw pi-times-circle',
-                                routerLink: ['/auth/error']
-                            },
-                            {
-                                label: 'Blood3',
-                                icon: 'pi pi-fw pi-lock',
-                                routerLink: ['/auth/access']
-                            }
-                        ]
-                    },
-
-                ]
-            },
             {
               label: 'History',
               items: [
-                  { label: 'History', icon: 'pi pi-question-circle', routerLink: ['/admin/QuestionHistory'] }
-
+                  { label: 'Question History', icon: 'pi pi-question-circle', routerLink: ['/admin/QuestionHistory'] },
+                  { label: 'History', icon: 'pi pi-hourglass', routerLink: ['/admin/History'] }
                   //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
                   //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
               ]
           },
-          {
-            label: 'Categories',
-            items: [
-                { label: 'category', icon: 'pi pi-copy', routerLink: ['/admin/Category'] }
 
-                //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-                //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
-            ]
-        },
-        {
-          label: 'Patients',
-          items: [
-              { label: 'Patient', icon: 'pi pi-user-plus', routerLink: ['/admin/Patient'] }
 
-              //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-              //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
-          ]
-      },
-      {
-        label: 'Address',
-        items: [
-            { label: 'Address', icon: 'pi pi-flag', routerLink: ['/admin/Address'] }
 
-            //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-            //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
+    //     {
+    //     label: 'Appointment',
+    //     items: [
+    //         { label: 'Appointment', icon: 'pi pi-clock', routerLink: ['/admin/Appointment'] }
 
-        ]
-    },
+    //         //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
 
-          {
-            label: 'Blood Equation',
-            items: [
-                { label: 'BloodEquation', icon: 'pi pi-arrow-right-arrow-left', routerLink: ['/admin/BloodEquation'] }
+    //         //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
-                //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
+    //     ]
+    // },
 
-                //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
 
-            ]
-        },
-        {
-            label: 'Department',
-            items: [
-                { label: 'Department', icon: 'pi pi-search', routerLink: ['/admin/Department'] }
-
-                //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-                //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-            ]
-        },
-        {
-            label: 'Department Hospital',
-            items: [
-                { label: 'Department_Hospital', icon: 'pi pi-search', routerLink: ['/admin/Department_Hospital'] }
-
-                //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-                //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-            ]
-        },
-        {
-            label: 'User',
-            items: [
-                { label: 'User', icon: 'pi pi-user', routerLink: ['/admin/User'] }
-
-                //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-                //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-            ]
-        },
-        {
-        label: 'Appointment',
-        items: [
-            { label: 'Appointment', icon: 'pi pi-clock', routerLink: ['/admin/Appointment'] }
-
-            //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-            //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-        ]
-    },
-    {
-      label: 'Historys',
-      items: [
-          { label: 'History', icon: 'pi pi-hourglass', routerLink: ['/admin/History'] }
-
-          //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-          //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-      ]
-  },
-  {
-    label: 'Hospital',
-    items: [
-        { label: 'Hospital', icon: 'pi pi-home', routerLink: ['/admin/Hospital'] }
-
-        //{ label: 'Invalid State', icon: 'pi pi-fw pi-exclamation-circle', routerLink: ['/uikit/invalidstate'] },
-
-        //{ label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'], routerLinkActiveOptions: { paths: 'subset', queryParams: 'ignored', matrixParams: 'ignored', fragment: 'ignored' } },
-
-    ]
-},
 
 
 
