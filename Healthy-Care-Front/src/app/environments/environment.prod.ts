3a68d9fd9d07ddbcedb03289ec81a29fbdcb2dad
osama mostafa <127859015+Osama0811@users.noreply.github.com>
2024-04-28 01:54:08 +0300

    Update environment.prod.ts

--- Healthy-Care-Front/src/app/environments/environment.prod.ts
+++ Healthy-Care-Front/src/app/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
  // BaseUrl:"http://localhost:23064/api",
-  BaseUrl:"http://www.healthycare2024.somee.com/api",
+  //BaseUrl:"http://www.healthycare2024.somee.com/api",
   //BaseUrl:"http://localhost:23064/api",
+  BaseUrl:"http://healthycareapi.runasp.net/api",
 
 };
